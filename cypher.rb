398947bd5f5d4bece5dfcd0b52cb5f7071c62909
sethherr <seth.william.herr@gmail.com>
2020-01-29 14:04:40 -0800

    ed first pass

--- cypher.rb
+++ cypher.rb
@@ -10,7 +10,53 @@
 #   - if bottom card is either joker, has value of 53
 
 class Cypher
-  def do_code
-    "code"
+  def self.initial_card_order
+    Array(1..52) + %w[a b]
+  end
+  
+  attr_accessor :deck
+
+  def initialize
+    @deck = self.class.initial_card_order
+  end
+
+  def joker_a
+    if @deck.last == "a"
+      @deck.pop
+      first = @deck.shift
+      @deck = [first, "a", *@deck]
+    else
+      a_position = @deck.index("a")
+      pre_a = @deck.slice(0, a_position)
+      one_after_a = @deck[a_position + 1]
+      after_one_after = @deck.slice(a_position + 2, 54 - a_position + 1)
+      @deck = pre_a + [one_after_a] + ["a"] + after_one_after
+    end
+  end
+
+  def joker_b_one_move
+    if @deck.last == "b"
+      @deck.pop
+      first = @deck.shift
+      @deck = [first, "b", *@deck]
+    else
+      b_position = @deck.index("b")
+      pre_b = @deck.slice(0, b_position)
+      one_after_b = @deck[b_position + 1]
+      after_one_after = @deck.slice(b_position + 2, 54 - b_position + 1)
+      @deck = pre_b + [one_after_b] + ["b"] + after_one_after
+    end
+  end
+
+  def joker_b
+    joker_b_one_move
+    joker_b_one_move
+  end
+
+  def triple_cut
+    b_position = @deck.index("b")
+    a_position = @deck.index("a")
+    first_joker = b_position > a_position ? a_position : b_position
+    second_joker = b_position > a_position ? b_position : a_position
   end
 end
--- test_cypher.rb
+++ test_cypher.rb
@@ -7,7 +7,46 @@ class TestCypher < Minitest::Test
     @cypher = Cypher.new
   end
 
-  def test_do_code_does_code
-    assert_equal "code", @code_thing.do_code
+  def test_initial_card_order
+    initial_order_manual = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "a", "b"]
+    assert_equal initial_order_manual, Cypher.initial_card_order
+    assert_equal initial_order_manual, @cypher.deck
+  end
+
+  def test_joker_a
+    first_move_result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "b", "a"]
+    @cypher.joker_a
+    assert_equal first_move_result, @cypher.deck
+    second_move_result = [1, "a", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "b"]
+    @cypher.joker_a
+    assert_equal second_move_result, @cypher.deck
+    # p @cypher.deck
+  end
+
+  def test_joker_b_basic
+    first_move_result = [1, 2, "b", 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "a"]
+    @cypher.joker_b
+    assert_equal first_move_result, @cypher.deck
+    second_move_result = [1, 2, 3, 4, "b", 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "a"]
+    @cypher.joker_b
+    assert_equal second_move_result, @cypher.deck
+    # p @cypher.deck
+  end
+
+  def test_joker_b_extra
+    @cypher.joker_a
+    @cypher.joker_b
+    move_result = [1, "b", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "a"]
+    assert_equal move_result, @cypher.deck
+    # p @cypher.deck
+  end
+
+  def test_triple_cut
+    @cypher.joker_a
+    @cypher.joker_b
+    @cypher.triple_cut
+    move_result = ["b", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "a", 1]
+    assert_equal move_result, @cypher.deck
+    # p @cypher.deck
   end
 end
